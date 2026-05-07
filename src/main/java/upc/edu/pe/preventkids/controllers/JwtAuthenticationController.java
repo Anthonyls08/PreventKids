db82bb562762cb0c08a3d73e160e459c52222a56
Dalí Paredes <dazps05@gmail.com>
2026-05-07 00:20:57 -0500

    web security config v3

--- src/main/java/upc/edu/pe/preventkids/controllers/JwtAuthenticationController.java
+++ src/main/java/upc/edu/pe/preventkids/controllers/JwtAuthenticationController.java
@@ -1,6 +1,7 @@
 package upc.edu.pe.preventkids.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -11,41 +12,41 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 import upc.edu.pe.preventkids.dtos.JwtRequestDTO;
 import upc.edu.pe.preventkids.dtos.JwtResponseDTO;
 import upc.edu.pe.preventkids.securities.JwtTokenUtil;
 import upc.edu.pe.preventkids.servicesimplements.JwtUserDetailsService;
 
-
-//Clase 3
 @RestController
 @CrossOrigin
 public class JwtAuthenticationController {
+
     @Autowired
     private AuthenticationManager authenticationManager;
+
     @Autowired
     private JwtTokenUtil jwtTokenUtil;
+
     @Autowired
     private JwtUserDetailsService userDetailsService;
 
-
     @PostMapping("/login")
-    public ResponseEntity<JwtResponseDTO> login(@RequestBody JwtRequestDTO req) throws Exception {
+    public ResponseEntity<JwtResponseDTO> login(@RequestBody JwtRequestDTO req) {
+
         authenticate(req.getUsername(), req.getPassword());
         final UserDetails userDetails = userDetailsService.loadUserByUsername(req.getUsername());
         final String token = jwtTokenUtil.generateToken(userDetails);
         return ResponseEntity.ok(new JwtResponseDTO(token));
     }
 
-    private void authenticate(String username, String password) throws Exception {
+    private void authenticate(String username, String password) {
         try {
             authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
         } catch (DisabledException e) {
-            throw new Exception("USER_DISABLED", e);
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "USER_DISABLED");
         } catch (BadCredentialsException e) {
-            throw new Exception("INVALID_CREDENTIALS", e);
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "INVALID_CREDENTIALS");
         }
-
-
     }
 }
